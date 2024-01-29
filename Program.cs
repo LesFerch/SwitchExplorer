136a9905bcf959283c75bf28c582971d3b013a9e
LesFerch <79026235+LesFerch@users.noreply.github.com>
2024-01-28 22:52:40 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace SwitchExplorer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            int buildNumber = int.Parse(Registry.GetValue(@"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion", "CurrentBuild", "").ToString());
+            if (buildNumber < 22631)
+            {
+                MessageBox.Show("Windows 11 23H2 or greater required", typeof(Program).Namespace);
+                return;
+            }
+
+            string E10A = @"HKEY_CURRENT_USER\Software\Classes\CLSID\{2aa9162e-c906-4dd9-ad0b-3d24a8eef5a0}";
+            string E10B = @"HKEY_CURRENT_USER\Software\Classes\CLSID\{6480100b-5a83-4d1e-9f69-8ae5a88e9a33}";
+
+            if ((Registry.GetValue(E10A, null, null) == null) && (Registry.GetValue(E10B, null, null) == null))
+            {
+                Registry.SetValue(E10A, "", "CLSID_ItemsViewAdapter", RegistryValueKind.String);
+                Registry.SetValue($"{E10A}\\InProcServer32", "", "C:\\Windows\\System32\\Windows.UI.FileExplorer.dll_", RegistryValueKind.String);
+                Registry.SetValue($"{E10A}\\InProcServer32", "ThreadingModel", "Apartment", RegistryValueKind.String);
+                Registry.SetValue(E10B, "", "File Explorer Xaml Island View Adapter", RegistryValueKind.String);
+                Registry.SetValue($"{E10B}\\InProcServer32", "", "C:\\Windows\\System32\\Windows.UI.FileExplorer.dll_", RegistryValueKind.String);
+                Registry.SetValue($"{E10B}\\InProcServer32", "ThreadingModel", "Apartment", RegistryValueKind.String);
+            }
+            else
+            {
+                try
+                {
+                    Registry.CurrentUser.DeleteSubKeyTree(@"Software\Classes\CLSID\{2aa9162e-c906-4dd9-ad0b-3d24a8eef5a0}", false);
+                    Registry.CurrentUser.DeleteSubKeyTree(@"Software\Classes\CLSID\{6480100b-5a83-4d1e-9f69-8ae5a88e9a33}", false);
+                }
+                catch 
+                {
+                }
+            }
+
+            var processes = Process.GetProcessesByName("explorer");
+            foreach (var process in processes)
+            {
+                process.Kill();
+                process.WaitForExit();
+            }
+
+            Process.Start("explorer.exe", AppDomain.CurrentDomain.BaseDirectory);
+
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # SwitchExplorer
-Switch Explorer between the Windows 11 version and the Windows 10 version
+
+[![image](https://user-images.githubusercontent.com/79026235/152910441-59ba653c-5607-4f59-90c0-bc2851bf2688.png)Download the zip file](https://github.com/LesFerch/SwitchExplorer/releases/download/1.0.0/SwitchExplorer.zip)
+
+## Switch the Windows 11 default Explorer
+
+This program switches (i.e. toggles) the Windows 11 default file manager back and forth between the the current version and the Windows 10 Explorer. That is, if your current Explorer is the Windows 11 version, double-clicking this program will switch to the Windows 10 version (the same one you can get to via the Control Panel). Double-clicking again will switch back to the current Windows 11 Explorer.
+
+The switch is accomplished by applying or removing the registry entries shown [here](https://www.elevenforum.com/t/restore-classic-file-explorer-with-ribbon-in-windows-11.620/#Three). You do not need to look at those registry entries. The link is provided as an FYI only.
+
+**Note**: SwitchExplorer.exe requires Windows 11 23H2.
+
+**Note**: When switched to the Windows 10 Explorer, the Details pane will still be the new Windows 11 version. If you want to get the old Details pane, that allows direct editing of metadata, you will need to directly launch the Windows 10 Explorer. You can do that with the [OldExplorer](https://lesferch.github.io/OldExplorer) tool.
+
+
+## How to Download and Run
+
+1. Download the zip file using the link above.
+2. Extract **SwitchExplorer.exe**.
+3. Right-click **SwitchExplorer.exe**, select Properties, check Unblock, and click OK.
+4. Optionally move **SwitchExplorer.exe** to the folder of your choice.
+5. Double-click **SwitchExplorer.exe** to toggle Explorer
+6. If you skipped step 3, then, in the SmartScreen window, click More info and then Run anyway.
+
+**Note**: Some antivirus software may falsely detect the download as a virus. This can happen anytime you download a new executable and may require extra steps to whitelist the file.
+
+
+\
+\
+[![image](https://user-images.githubusercontent.com/79026235/153264696-8ec747dd-37ec-4fc1-89a1-3d6ea3259a95.png)](https://github.com/LesFerch/SwitchExplorer)
